a11f2f401af8ccb42b9d0884aa1751b476340fc4
Silviq Dimitrova <145548168+SisDimitrova@users.noreply.github.com>
2024-03-01 10:36:03 +0200

    Add files via upload

---
+++ AbstactionExercises/CardWithPower_03/Main.java
@@ -0,0 +1,17 @@
+package AbstractionExercisesCardWithPower_03;
+
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        CardRank cardRank = CardRank.valueOf(scanner.nextLine());
+        CardSuit cardSuit = CardSuit.valueOf(scanner.nextLine());
+
+        int cardPower = cardRank.getPower() + cardSuit.getPower();
+
+        System.out.printf("Card name: %s of %s; Card power: %d",cardRank, cardSuit, cardPower);
+    }
+}
