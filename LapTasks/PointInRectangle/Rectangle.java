5548eed3440b2eee49bab88fe092a3b5b1ce86f1
Silviq Dimitrova <145548168+SisDimitrova@users.noreply.github.com>
2024-03-01 10:25:27 +0200

    Add files via upload

---
+++ LapTasks/PointInRectangle/Rectangle.java
@@ -0,0 +1,27 @@
+package PointInRectangle_02;
+
+public class Rectangle {
+    private Point bottomLeft;
+    private Point topRight;
+
+    public Rectangle(Point bottomLeft, Point topRight) {
+        this.bottomLeft = bottomLeft;
+        this.topRight = topRight;
+    }
+
+    public Point getBottomLeft() {
+        return bottomLeft;
+    }
+
+    public Point getTopRight() {
+        return topRight;
+    }
+    public boolean contains(Point point) {
+        boolean isInVertical =this.getBottomLeft().getY() <= point.getY() &&
+                this.getTopRight().getY() >= point.getY();
+        boolean isInHorizontal = this.getBottomLeft().getX() <= point.getX() &&
+                this.getTopRight().getX() >= point.getX();
+
+       return isInHorizontal && isInVertical;
+    }
+}
