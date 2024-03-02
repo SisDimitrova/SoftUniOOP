1dc298b34efea22418c0e82d07e8c1ade98ff040
Silviq Dimitrova <145548168+SisDimitrova@users.noreply.github.com>
2024-03-02 07:50:53 +0200

    Add files via upload

---
+++ LapTasks/CarShopExtendIAA/Rentable.java
@@ -0,0 +1,6 @@
+package CarShopExtendInterfaceAndAbstraction;
+
+public interface Rentable extends Car {
+    Integer getMinRentDay();
+    Double getPricePerDay();
+}
---
+++ LapTasks/CarShopExtendIAA/Sellable.java
@@ -0,0 +1,5 @@
+package CarShopExtendInterfaceAndAbstraction;
+
+public interface Sellable extends Car {
+    Double getPrice();
+}
